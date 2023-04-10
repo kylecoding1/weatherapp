00417ff739b52bc9bacd5aa62a79edf155a4ee88
kylecoding1 <128002901+kylecoding1@users.noreply.github.com>
2023-04-10 16:27:30 -0700

    js finished

--- script.js
+++ script.js
@@ -47,3 +47,45 @@ function fetchForecast(city) {
     .then(response => response.json())
     .then(data => displayForecast(data));
 }
+// Display current weather data in the DOM
+function displayCurrentWeather(data) {
+    const { name, main, weather, wind } = data;
+    const date = new Date().toLocaleDateString();
+    const icon = `https://openweathermap.org/img/wn/${weather[0].icon}.png`;
+  
+    currentWeatherDiv.innerHTML = `
+      <h2>${name} (${date})</h2>
+      <img src="${icon}" alt="${weather[0].description}">
+      <p>Temperature: ${main.temp}°C</p>
+      <p>Humidity: ${main.humidity}%</p>
+      <p>Wind Speed: ${wind.speed} m/s</p>
+    `;
+  }
+  
+  // Display 5-day forecast data in the DOM
+  function displayForecast(data) {
+    // Filter the data to only include forecasts at 12:00 PM
+    const dailyData = data.list.filter(item => item.dt_txt.includes("12:00:00"));
+    let forecastHTML = "";
+  
+    // Generate HTML for each day in the forecast
+    dailyData.forEach(day => {
+      const date = new Date(day.dt_txt).toLocaleDateString();
+      const icon = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+  
+      forecastHTML += `
+        <div class="forecast-day">
+          <h3>${date}</h3>
+          <img src="${icon}" alt="${day.weather[0].description}">
+          <p>Temperature: ${day.main.temp}°C</p>
+          <p>Humidity: ${day.main.humidity}%</p>
+          <p>Wind Speed: ${day.wind.speed} m/s</p>
+        </div>
+      `;
+    });
+  
+    // Update the DOM with the generated forecast HTML
+    forecastDiv.innerHTML = forecastHTML;
+  }
+  
+  
